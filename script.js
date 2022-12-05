cb61081161326c4c400b0da5bf6f5da9cc334c13
kashkaffie <kashbalogun1@gmail.com>
2022-12-05 14:51:25 +0100

    project done

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+ $(document).ready(function(){
+//     $("#btn").click(function(){
+//         $("#img").attr("src", "./images/car1.jpg" ).hide(5000)
+//     })
+
+// $("#btn").click(function(){
+//   $("#header").toggleClass("toggler")
+// })
+
+// $(".fa-sharp").click(function(){
+//     $("#input").attr("type", "text")
+// })
+
+$("#btn").click(function(){
+    $(".test").load("readme.txt")
+})
+
+ })
+
+
---
+++ script2.js
@@ -0,0 +1,5 @@
+import {person1} from "./script.js"
+
+var newPerson = {...person1, city: "ijebu"}
+
+console.log(newPerson)
